7d15e8575c5ecbe46744607164b4d8ca2cf84193
Danilo Albuquerque <danilosk10@hotmail.com>
2019-01-16 12:14:40 -0200

    funções click
    adicionado a função click, data e hora.

--- scripts/controller/CalController.js
+++ scripts/controller/CalController.js
@@ -1,38 +1,91 @@
 class CalController {
 
 constructor(){
-
+    this._locale = 'pt-BR';
     this._displayCalcEl = document.querySelector("#display");
     this._dateEl = document.querySelector("#data");
     this._timeEl = document.querySelector("#hora");
     this.currentDate;
     this.initialize();
+    this. initButtonEvents();
 }
 
 initialize(){
-    
-    this._dateEl.innerHTML = "15/01/2019";
-    this._timeEl.innerHTML = "15:53";
+    /*
+    executa um intervalo em milisegundos
+   setInterval(() => {
+       
+   }, interval); */
+   setInterval(() => {
+       this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+        day:"2-digit",
+        month: "long",
+        year: "numeric"
 
+       });
+       this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+}, 1000);
 }
+
+    initButtonEvents(){
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g ");
+            buttons.forEach((btn, index)=>{
+
+                btn.addEventListener('click', e=> {
+
+                    console.log(btn.className.baseVal.replace("btn-",""));
+
+                })
+
+            })
+    }
+
+        get displayTime(){
+
+            return this._timeEl.innerHTML;
+
+        }
+
+        set displayTime(value){
+
+            this._timeEl.innerHTML = value;
+
+        }
+        
+        get displayDate(){
+
+            return this._dateEl.innerHTML;
+
+        }
+
+        set displayDate(value){
+
+            this._dateEl.innerHTML = value;
+
+        }
+
+
     get displayCalc(){
-
-        return this._displayCalcEL.innerHTML;
-
+        return this._displayCalcEl.innerHTML;
     }
 
-    set displayCalc(valor){
-        this._displayCalcEL.innerHTML = valor;
-
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
     }
+/* dt.toLocaleDateString('pt-BR',{month:'short'}) puxa o mes abreviado, 
+    dt.toLocaleDateString('pt-BR',{month:'long'}) puxa o mes por extenso em portugues
+    dt.toLocaleTimeString('pt-BR') puxa a hora completa 
+    dt = variavel declarado ao new Date() -- dt.getDate() puxa somente a data
+    dt.getMonth() puxa somente o mes
+    dt.getFullYear() puxa somente o ano
 
+*/
     get currentDate(){
-
-        return this._currentDate;
-
+        return new Date();
     }
 
-    set currentDate(valor){
-        this._currentDate = valor;
+    set currentDate(value){
+        this._currentDate = value;
     }
 }
