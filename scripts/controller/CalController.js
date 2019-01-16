82ba0cddae4fc2980a1930f229bfc06647eb5252
Danilo Albuquerque <danilosk10@hotmail.com>
2019-01-16 18:47:43 -0200

    Update CalController.js
    adicionado os eventos de clique, e mudança do stylo do cursor do mouse.

--- scripts/controller/CalController.js
+++ scripts/controller/CalController.js
@@ -26,19 +26,32 @@ initialize(){
        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
 }, 1000);
 }
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => { 
+
+            element.addEventListener(event, fn, false);
+        });
+
+    }
 
     initButtonEvents(){
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g ");
             buttons.forEach((btn, index)=>{
 
-                btn.addEventListener('click', e=> {
+                this.addEventListenerAll(btn, "click drag ", e=> {
 
                     console.log(btn.className.baseVal.replace("btn-",""));
 
-                })
+                });
 
-            })
+                this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+                    
+                    btn.style.cursor = "pointer"; 
+
+                });
+
+            });
     }
 
         get displayTime(){
